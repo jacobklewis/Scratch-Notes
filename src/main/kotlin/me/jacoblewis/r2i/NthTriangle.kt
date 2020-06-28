fe8b1c29cbbb9d0929c671537511df57a57139c0
jacobklewis <zerogamerfx@gmail.com>
2020-06-28 14:32:16 -0500

    added clearer documentation to the NthTriangle.kt functions

--- src/main/kotlin/me/jacoblewis/r2i/NthTriangle.kt
+++ src/main/kotlin/me/jacoblewis/r2i/NthTriangle.kt
@@ -5,11 +5,16 @@ fun main() {
     println(nthTriangleIter(4))
 }
 
+/**
+ * @param n: A Natural Number [0-inf]
+ *
+ * @return The nth triangle number
+ */
 fun nthTriangleIter(n: Long): Long {
-    if (n < 2) {
-        return n
-    }
-    return (1..n).reduce { acc, l -> acc + l }
+    return (0..n).reduce { acc, l -> acc + l }
+}
+fun nthTriangleIter2(n: Long): Long {
+    return (0..n).sum()
 }
 
 
